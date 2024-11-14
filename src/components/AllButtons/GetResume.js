80a8d1aaa4b01dae94f507a960f927a8daab4544
Ankit <ankitvika02@gmail.com>
2024-11-14 13:42:12 +0530

    update: changes in resume link

--- src/components/AllButtons/GetResume.js
+++ src/components/AllButtons/GetResume.js
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 const GetResume = () => {
     return (
         <>
-            <NavLink to={"https://drive.google.com/file/d/1Cjf9iB8yYn71HmaQrvdMB8iiKJWugj9W/view?usp=sharing"} target="_blank" className="btn_container">
+            <NavLink to={"https://drive.google.com/file/d/1lXx-yKSGgO4G31qp9XVrcizbE28RWBlf/view?usp=sharing"} target="_blank" className="btn_container">
             <motion.div
             whileTap={{
               scale: 0.90
